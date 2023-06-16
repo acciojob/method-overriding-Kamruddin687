ff913ff6a69a2697304a03172f47a38aa21f353a
kamruddin678 <kamruddincut@gmail.com>
2023-06-16 17:37:13 +0530

    To change in overriden

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,15 +2,15 @@ package com.driver;
 
 public class Main {
   public  static  class A{
-      public  String meth(){
-          return "Invoking method from class A";
+      public String meth()
+      { return "Invoking method from class A";
       }
 
   }
   public  static  class B extends A{
       public String meth()
       {
-          return "Method is overridden in Extend class B";
+          return "Method is overridden in Extended class B";
       }
   }
   public  static void main(String[] args){
